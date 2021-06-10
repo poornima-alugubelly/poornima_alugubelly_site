abfdf86da1e8c68a11dde8b35f7d62b5655ba3cf
poornima-alugubelly <reddypoornima683@gmail.com>
2021-06-10 17:30:53 +0530

    refactor : removed unnecessary js code which was inside head part

--- dark-mode.js
+++ dark-mode.js
@@ -1,5 +1,4 @@
-// let darkMode = localStorage.getItem('darkMode'); 
-
+let darkMode = localStorage.getItem('darkMode'); 
 const darkModeToggle = document.querySelector('#icon');
 
 const enableDarkMode = () => {
